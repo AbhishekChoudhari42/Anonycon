320ccaa915f69283e052184f1fe4a98d0666294f
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2022-12-21 14:22:32 +0530

    first commit

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  username:{
+    type:String,
+    require:true,
+    min:2,
+    max:120,
+    unique:true,
+  },
+  email:{
+    type:String,
+    require:true,
+    min:2,
+    max:180,
+    unique:true,
+  },
+  password:{
+    type:String,
+    min:8,
+    max:80,
+    unique:true,
+  },
+
+  message : {
+    type : Array,
+    default:[]
+  }
+},{ timestamps: true })
+
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv")
+dotenv.config();
+const express = require('express')
+const cors = require('cors')
+const cookieSession = require('cookie-session')
+const mongoose = require('mongoose')
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+const app = express()
+
+
+// connection
+async function connect(){
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true})
+        console.log("connected to db")
+    }catch(error){
+        console.error(error)
+    }
+}
+connect()
+
+mongoose.set('strictQuery', true);
+
+const port = process.env.PORT || 5000
+    app.listen(port, () => console.log("💥"))
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+
+const userRouter = require("./routes/user")
+app.use("/user",userRouter)
+
+
+
+
