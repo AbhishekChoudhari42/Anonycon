75be79cf142ae10b7be3e94dd210869921f1e3e3
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2022-12-22 12:48:44 +0530

    frontend

---
+++ client/frontend/src/pages/Receiver.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Message from '../components/Message'
+import Nav from '../components/Nav'
+import { useState , useEffect } from 'react'
+
+const Receiver = () => {
+  return (
+    <div>
+        <Nav/>
+        <div className="main">
+            <div className='link'>
+                
+            </div>
+            <div className='msg-area'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Receiver
---
+++ client/frontend/src/pages/Sender.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sender = () => {
+  return (
+    <div>
+        <div className="texting-box">
+
+        </div>
+    </div>
+  )
+}
+
+export default Sender
